b07d4c18a069c6bb172a471d11b9fb5356fe4a32
Logan Schaefer <loganrayschaefer04@gmail.com>
2025-03-03 19:55:34 -0500

    Implemented CardSelector and Card class

--- src/main/java/blackjack/Card.java
+++ src/main/java/blackjack/Card.java
@@ -1,4 +1,54 @@
 package blackjack;
+import javafx.scene.image.Image;
 
 public class Card {
+    String suit;
+    String rank;
+    int value;
+    private Image image;
+
+    /**
+     * Constructor for each card
+     * @param rank String holding rank of card.
+     * @param suit String holding suit of card.
+     * @param value - integer holding value of card.
+     */
+    public Card(String rank, String suit, int value) {
+        this.suit = suit;
+        this.rank = rank;
+
+        if (value >= 9) {
+            this.value = 10;
+        } else if (value == 0) {
+            this.value = 0;
+        } else {
+            this.value = value + 1;
+        }
+
+        // These lines need to stay commented out until we have all the card images ready in resources.
+        // String imageName = rank + suit;
+        // Image image = new Image("src/main/resources/CardImages/" + imageName);
+    }
+
+    /** Return the value of card.
+     *
+     * @return Value of each card. Returns 0 if card is an ace.
+     */
+    public int getValue() {
+        return value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public Image getImage() {
+        // Continues to return null until images are in resources.
+        // return image;
+        return null;
+    }
 }
--- src/main/java/blackjack/CardSelector.java
+++ src/main/java/blackjack/CardSelector.java
@@ -1,4 +1,43 @@
 package blackjack;
+import java.util.Random;
 
 public class CardSelector {
+    private String cardSelectorType;
+    private static final String[] ranks = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+            "Jack", "Queen", "King"};
+    private static final String[] suits = {"Heart", "Diamond", "Club", "Spade"};
+
+
+    /** CardSelector Constructor
+     * Saves user settings for card generation settings
+     * @param cardSelectorType - the way in which cards will be generated.
+     */
+    public CardSelector(String cardSelectorType) {
+        // This constructor will remain empty for now.
+        // For now, all cards will be generated randomly.
+        //this.cardSelectorType = cardSelectorType;
+    }
+
+    /** Checks card generation settings and calls necessary method for generation.
+     * @return Next card instance.
+     */
+    public Card getNextCard() {
+        // This method will call another method only right now.
+        // This will be updated as we continue in the project with different card generation styles.
+        return getRandomCard();
+    }
+
+    /**
+     * Generates a random card based on random number generator.
+     * @return a random card object.
+     */
+    private Card getRandomCard() {
+        Random randomNumberGenerator = new Random();
+
+        int rankNumber = randomNumberGenerator.nextInt(12);
+        int suitNumber = randomNumberGenerator.nextInt(3);
+
+        return new Card(ranks[rankNumber], suits[suitNumber], rankNumber);
+    }
+
 }
