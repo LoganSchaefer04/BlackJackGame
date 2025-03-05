7f8010e1e04d7b8ccb65c7f6134ca2f76a8072f1
Logan Schaefer <loganrayschaefer04@gmail.com>
2025-03-05 13:31:02 -0500

    Fixed javadocs for Ace constructor
    Changed the name of count to value to mimic the other methods using it.

--- src/main/java/blackjack/Ace.java
+++ src/main/java/blackjack/Ace.java
@@ -9,18 +9,19 @@ public class Ace extends Card {
      *
      * @param rank String holding rank of card.
      * @param suit String holding suit of card.
+     * @param value The value of the hand at current time.
      */
-    public Ace(String rank, String suit, int count) {
+    public Ace(String rank, String suit, int value) {
         super(rank, suit);
         // Check if player would bust if ace is counted as 11.
-        if (count >= 11) {
+        if (value >= 11) {
             // Card is worth 1.
-            value = 1;
+            this.value = 1;
 
         // Player would not bust if ace is counted as 11.
         } else {
             // Card is worth 11.
-            value = 11;
+            this.value = 11;
         }
     }
 
