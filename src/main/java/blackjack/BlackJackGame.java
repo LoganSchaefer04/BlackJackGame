6622a2f56dfeca2bfbf5bf7d575faa202274f270
ljpickart <ljpickar@mtu.edu>
2025-03-05 20:49:05 -0500

    Created player methods, and implemented them into the blackjack text based game in blackJackGame.java

--- src/main/java/blackjack/BlackJackGame.java
+++ src/main/java/blackjack/BlackJackGame.java
@@ -2,6 +2,7 @@ package blackjack;
 import java.util.*;
 public class BlackJackGame {
     private Dealer dealer;
+    private Player player;
     private CardSelector cardSelector;
     private boolean gameOver;
     private Scanner scanner;
@@ -12,14 +13,15 @@ public class BlackJackGame {
         dealer = new Dealer(cardSelector);
         scanner = new Scanner(System.in);
         gameOver = false;
+        player = new Player(cardSelector);
     }
 
     public void initRound() {
         System.out.printf("\n ---------- ROUND %d ----------\n", roundCounter);
         roundCounter++;
         dealer.initHand();
+        player.initHand();
         gameOver = false;
-
     }
 
     public void dealerTurn(){
@@ -30,7 +32,48 @@ public class BlackJackGame {
     }
 
     public void determineWinner(){
+        if(dealer.hasBust() && player.hasBust()) {
+            System.out.println("No winner, both bust.");
+            return;
+        }
 
+        if(dealer.hasBust() && !player.hasBust()) {
+            System.out.println("Player wins!");
+            return;
+        }
+
+        if(!dealer.hasBust() && player.hasBust()) {
+            System.out.println("Dealer wins!");
+            return;
+        }
+
+        if(dealer.getHandVal() > player.getHandValue()) {
+            System.out.println("Dealer wins!");
+        } else {
+            System.out.println("Player wins!");
+        }
+    }
+
+    public void playerTurn() {
+        while(true) {
+            System.out.println("Current Player Hand Value: " + player.getHandValue());
+            player.printHand();
+            System.out.println("Would you like to hit or stay?\nh = hit\ns = stay");
+            String input = scanner.nextLine();
+
+            if(input.equals("h")) {
+                if(player.hit()) {
+                    System.out.println("Player BUST, turn over");
+                    break;
+                } else {
+                    System.out.println("New hand value: " + player.getHandValue());
+                }
+            } else if (!input.equals("s")) {
+                System.out.println("Invalid input, try again");
+            } else {
+                break;
+            }
+        }
     }
 
     public boolean askPlayAgain() {
@@ -57,7 +100,7 @@ public class BlackJackGame {
         while(stillPlaying){
             initRound();
             if(!gameOver){
-                //playerTurn
+                playerTurn();
             }
             if(!gameOver){
                 dealerTurn();
--- src/main/java/blackjack/Player.java
+++ src/main/java/blackjack/Player.java
@@ -1,5 +1,92 @@
 package blackjack;
 
+import java.util.ArrayList;
+
+/**
+ * Class for players and their actions
+ */
 public class Player {
+    private int handValue;
+    private ArrayList<Card> hand = null;
+    private CardSelector dealer;
+
+    /**
+     * Constructor
+     * @param dealer, used to generate new cards when needed
+     */
+    Player(CardSelector dealer) {
+        this.dealer = dealer;
+        handValue = 0;
+        hand = new ArrayList<Card>();
+    }
+
+    /**
+     * Calculates the value of hand
+     * @return int, value of the hand
+     */
+    private int calcHand() {
+        int value = 0;
+        for(Card c : hand) {
+            value += c.getValue();
+        }
+        return value;
+    }
+
+    /**
+     * Checks if hand is bust or not
+     * @return true if hand is over 21, else returns false
+     */
+    private boolean checkBust() {
+        if(calcHand() > 21) {return true;}
+        return false;
+    }
+
+    /**
+     *
+     * @return true if bust after hit, else return false
+     */
+    public boolean hit() {
+        hand.add(dealer.getNextCard(handValue));
+        handValue = calcHand();
+
+        return checkBust();
+    }
+
+    /**
+     * Clears any previous hand, creates a new initial hand with 2 cards in it
+     */
+    public void initHand() {
+        hand.clear();
+        hand.add(dealer.getNextCard(handValue));
+        hand.add(dealer.getNextCard(handValue));
+        handValue = calcHand();
+    }
+
+    /**
+     * get value of hand
+     * @return int, value of hand
+     */
+    public int getHandValue() {
+        return handValue;
+    }
+
+    /**
+     * checks if player bust
+     * @return boolean, true if bust, false otherwise
+     */
+    public boolean hasBust() {
+        return checkBust();
+    }
+
+    /**
+     * print hand, prints out the hand of the user with rank and suit
+     */
+    public void printHand() {
+        System.out.println("Players hand of cards: ");
+        for(Card c : hand) {
+            System.out.print(c.getRank() + " of " + c.getSuit() + ", ");
+        }
+        System.out.print("\n");
+    }
 
 }
