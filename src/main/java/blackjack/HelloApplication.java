1594f2e3646e2804241e35efa0bf5acbb7610041
Aidan Boop <slushydog15@gmail.com>
2025-03-12 11:44:52 -0400

    Update HelloApplication.java to BlackJackApplication.

--- src/main/java/blackjack/HelloApplication.java
+++ src/main/java/blackjack/HelloApplication.java
@@ -8,14 +8,14 @@ import javafx.scene.Parent;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class BlackJackApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Blackjack Game.fxml"));
         BlackJackGame blackJackGame = new BlackJackGame();
         fxmlLoader.setController(new BJController(blackJackGame));
         Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(root, 700, 450);
         stage.setScene(scene);
         stage.show();
     }
